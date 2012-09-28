c81a5e7754b93808b9a1877f3385c465caca4e86
johnsonthomas1974 <johnsonthomas1974@gmail.com>
2012-09-28 17:15:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-This attachment was removed.
+var http = require("http");
+
+function onRequest(request, response) {
+  console.log("Request received. 1");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}
+
+http.createServer(onRequest).listen(80);
+
+console.log("Server has started.");.
