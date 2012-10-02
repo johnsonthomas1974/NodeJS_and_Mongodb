67de0b9366c9aae76e5c3728d3585eeb877abc0b
Johsnon Thomas <johnsonthomas1974@gmail.com>
2012-10-02 13:29:16 -0500

    Signed-off-by: Signedoff <johnsonthomas1974@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require(‘express’); 
+var express = require("express"); 
 var app = express.createServer(); 
 var port = process.env.PORT || 3000;
 
-app.get(‘/’, function(request, response) { 
-  response.send(‘Hello Engine Yard Cloud!’);
+app.get("/", function(request, response) { 
+  response.send("Hello Engine Yard Cloud!");
 });
 
 app.listen(port);
