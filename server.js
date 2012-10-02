eb8af655446a846ce758679e89705ae491edb0c5
Johsnon Thomas <johnsonthomas1974@gmail.com>
2012-10-02 14:39:26 -0500

    Signed-off-by: Johsnon Thomas <johnsonthomas1974@gmail.com>

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require("express"); 
 var app = express.createServer(); 
 var port = process.env.PORT || 3000;
@@ -7,3 +8,13 @@ app.get("/", function(request, response) {
 });
 
 app.listen(port);
+*/
+var http = require("http");
+var port = process.env.PORT || 3000;
+var webServer = http.createServer(function (request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}  );
+webServer.listen(port);
+
